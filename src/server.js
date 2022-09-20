35e59dd3069d20e6db19252a70768b7b0273819f
ErkkoV <erkko.vallivaara@gmail.com>
2022-09-20 12:27:42 +0300

    added simple chat

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 const httpServer = http.createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://10.201.204.39:3000",
         // origin: "http://10.69.168.88:3000",
     },
 });
@@ -17,6 +17,9 @@ io.on("connection", (socket) => {
     });
     socket.on("rolls-front", (args) => {
         io.to("noppasivu").emit("rolls-back", args);
+    });
+    socket.on("messages-front", (args) => {
+        io.to("noppasivu").emit("messages-back", args);
     });
 });
 
