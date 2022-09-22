e8ab2603f0a6e3122f07a1099354423286b219e3
ErkkoV <erkko.vallivaara@gmail.com>
2022-09-22 08:33:01 +0300

    creates datgabase entries for rolls, probs and messages

--- src/database.js
+++ src/database.js
@@ -8,22 +8,23 @@ const pool = new pg.Pool({
     port: 5433,
 });
 
-pool.query("SELECT NOW()", (err, res) => {
+const probtext =
+    'CREATE TABLE IF NOT EXISTS public."Probs"("ID" "char" NOT NULL, "UserID" "char" NOT NULL, "AttackSkill" integer,"DefenceSkill" integer,"AttackRoll" integer,"DefenceRoll" integer,"Results" json[])';
+const rolltext =
+    'CREATE TABLE IF NOT EXISTS public."Rolls"("ID" "char" NOT NULL, "UserID" "char" NOT NULL, "AttackSkill" integer,"DefenceSkill" integer,"AttackRoll" integer,"DefenceRoll" integer,"Results" json[])';
+const messagetext =
+    'CREATE TABLE IF NOT EXISTS public."Messages"(message "char" NOT NULL)';
+
+pool.connect();
+
+pool.query(probtext, (err, res) => {
     console.log(err, res);
-    pool.end();
 });
 
-const client = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "Noppasivut",
-    password: "kakka",
-    port: 5433,
+pool.query(rolltext, (err, res) => {
+    console.log(err, res);
 });
 
-client.connect();
-
-client.query("SELECT NOW()", (err, res) => {
+pool.query(messagetext, (err, res) => {
     console.log(err, res);
-    client.end();
 });
